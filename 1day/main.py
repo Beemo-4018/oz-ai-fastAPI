396ddd52346889f877c2950445e6014a98780f3b
ico2604 <ico2604@naver.com>
2026-02-23 18:06:55 +0900

    1일차

---
+++ 1day/main.py
@@ -0,0 +1,19 @@
+﻿from fastapi import FastAPI
+
+app = FastAPI()
+
+users = [
+    {"id": 1, "name": "Alice"},
+    {"id": 2, "name": "Bob"},
+    {"id": 3, "name": "Charlie"},
+]
+
+
+@app.get("/users")
+def get_users_handlers():
+    return users
+
+
+@app.get("/users/{user_id}")
+def get_user_handler(user_id: int):
+    return users[user_id - 1]
